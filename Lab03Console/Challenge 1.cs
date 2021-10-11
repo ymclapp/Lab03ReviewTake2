4dbd9283e77d30810d97d77be1f0c03ec2665e76
ymclapp <ymclapp@yahoo.com>
2021-10-10 20:42:39 -0500

    worked on challenge 1

---
+++ Lab03Console/Challenge 1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03Console
+{
+  class Challenge_1
+  {
+    public void Product(int numbers)
+    {
+      Console.WriteLine("Please enter three numbers separated by a space");
+    }
+  }
+}
--- Lab03Console/Program.cs
+++ Lab03Console/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+using System;
 
 namespace Lab03Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+      int userInput, userInput1, userInput2, userInput3, product;
+      String strValue, hardCoded;
+
+      Console.WriteLine("Please enter a number:  ");
+      strValue = Console.ReadLine();
+      userInput1 = Convert.ToInt32(strValue);
+
+      Console.WriteLine("Please enter another number:  ");
+      strValue = Console.ReadLine();
+      userInput2 = Convert.ToInt32(strValue);
+
+      Console.WriteLine("Please enter your last number:  ");
+      strValue = Console.ReadLine();
+      userInput3 = Convert.ToInt32(strValue);
+
+      product = userInput1 * userInput2 * userInput3;
+      Console.WriteLine($"The product of those numbers is: {product} ");
+
+      hardCoded = "1 2 3";
+      //Console.WriteLine("Please enter three numbers separated by a space (ex. 1 2 3):  ");
+      //strValue = Console.ReadLine();
+      String [] hardCode = hardCoded.Split(" ");
+      //userInput = Convert.ToInt32(inputSplit);
+      foreach(var number in hardCode)
+      {
+        Console.WriteLine(hardCode);
+      }
+     
+
         }
     }
 }
